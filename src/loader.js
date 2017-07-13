9d880ff61ca820c907d18b58829b36cac1969d6e
Anton Shvab <shwab.ay@gmail.com>
2017-07-13 16:59:20 +0300

    fix

--- src/loader.js
+++ src/loader.js
@@ -97,56 +97,5 @@ const load = (url, output) => {
         results.map(([resp, _path, responseType]) => writes[responseType](resp, _path)));
     });
 };
-  // axios
-//   .get(url, {
-//     headers: {
-//       'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
-//     },
-//   })
-//   .then((res) => {
-//     const { hostname, pathname } = new URL(url);
-//     const hostnamePart = hostname.split('.');
-//     const pathnamePart = pathname === '/' ? [] : pathname.slice(1).split('/');
-//     const samePart = [...hostnamePart, ...pathnamePart].join('-');
-//     const htmlFilename = [samePart, '.html'].join('');
-//     const assetsDirname = [samePart, '_files'].join('');
-//     const htmlPath = path.join(output, htmlFilename);
-//     const assetsPath = path.join(output, assetsDirname);
-
-//     const pathAndOpts = new Map();
-
-//     const html = res.data;
-//     const $ = cheerio.load(html);
-//     const elemsWithAssets = $('script[src^="/"], link[href^="/"], img[src^="/"], meta[content^="/"]');
-//     elemsWithAssets.each((i, elem) => {
-//       const [currAssetPath, attrib] = [
-//         [$(elem).attr('src'), 'src'],
-//         [$(elem).attr('href'), 'href'],
-//         [$(elem).attr('content'), 'content'],
-//       ].find(([_path]) => _path !== undefined);
-
-//       const extname = path.extname(currAssetPath).slice(1);
-//       const newRelativeAssetPath = path.format({
-//         dir: assetsDirname,
-//         base: currAssetPath.slice(1).split('/').join('-'),
-//       });
-
-//       $(elem).attr(attrib, newRelativeAssetPath);
-//       const newAbsoluteAssetPath = path.resolve(output, newRelativeAssetPath);
-//       pathAndOpts.set(newAbsoluteAssetPath, {
-//         method: 'get',
-//         url: new URL(currAssetPath, url).href,
-//         responseType: respType[extname],
-//       });
-//     });
-//     const localHtml = $.html();
-
-//     return Promise.all([pathAndOpts, mkdir(assetsPath), writeFile(htmlPath, localHtml)]);
-//   })
-//   .then(([pathAndOpts]) => Promise.all(
-//     Array.from(pathAndOpts)
-//       .map(([_path, opt]) => Promise.all([axios(opt), _path, opt.responseType]))))
-//   .then(results => Promise.all(
-//     results.map(([resp, _path, responseType]) => writes[responseType](resp, _path))));
 
 export default load;
